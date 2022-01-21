dc0dc243f2219907d8c97d56c34296e1a7ed6c7e
SandeshJIT <hssandesh111@gmail.com>
2022-01-21 15:20:42 +0530

    new changes v1.0

--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,54 @@
 const express = require("express")
 const router = express.Router()
-
+const app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser());
+const cors = require('cors');
 router.use(logger)
+app.use(cors());
 
-router.get("/", (req, res) => {
+// app.set('view engine', 'jade');
+app.listen((3000), ()=>{
+    console.log("listening")
+})
+
+app.get("/", (req, res) => {
   console.log(req.query.name)
-  res.send("User List")
+  res.send(users)
 })
 
-router.get("/new", (req, res) => {
-  res.render("users/new")
+app.get("/new", (req, res) => {
+  res.sendFile("C:/Users/user/Desktop/Personal Projects/express-crash-course/views/users/new.html")
 })
 
-router.post("/", (req, res) => {
-  const isValid = false
-  if (isValid) {
-    users.push({ firstName: req.body.firstName })
+app.post("/users", (req, res) => {
+  // const isValid = false
+  // if (isValid) {
+    console.log(req.body)
+    users.push({ name: req.body.firstName })
     res.redirect(`/users/${users.length - 1}`)
-  } else {
-    console.log("Error")
-    res.render("users/new", { firstName: req.body.firstName })
-  }
+  // } else {
+  //   console.log("Error")
+  //   res.render("users/new", { firstName: req.body.firstName })
+  // }
 })
-
-router
-  .route("/:id")
-  .get((req, res) => {
-    console.log(req.user)
-    res.send(`Get User With ID ${req.params.id}`)
+app.param("id", (req, res, next, id) => {
+  req.user = users[id]
+  next()
+})
+app.get("/users/:id",(req, res) => {
+    console.log(req.user.name)
+    res.send(`Get User With ID ${req.params.id} Name is ${req.user.name}`)
   })
-  .put((req, res) => {
+  .put("/users/:id",(req, res) => {
     res.send(`Update User With ID ${req.params.id}`)
   })
-  .delete((req, res) => {
+  .delete("/users/:id",(req, res) => {
     res.send(`Delete User With ID ${req.params.id}`)
   })
 
 const users = [{ name: "Kyle" }, { name: "Sally" }]
-router.param("id", (req, res, next, id) => {
-  req.user = users[id]
-  next()
-})
+
 
 function logger(req, res, next) {
   console.log(req.originalUrl)
